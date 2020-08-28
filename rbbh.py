ed19908b52922d4b1c830694112e8d6684b1e3ee
Dominik R Laetsch <dominik.laetsch@gmail.com>
2020-08-28 18:08:41 +0100

    Added docstring

--- rbbh.py
+++ rbbh.py
@@ -7,8 +7,8 @@ Usage:
 
 Options:
        -h --help                                show this
-       -1, --input_dir1 <DIR>                   Input directory of proteome 1
-       -2, --input_dir2 <DIR>                   Input directory of proteome 2
+       -1, --input_dir1 <DIR>                   Input directory of proteome 1 (BLAST result, query FASTA)
+       -2, --input_dir2 <DIR>                   Input directory of proteome 2 (BLAST result, query FASTA)
        -o, --outprefix <STRING>                 Output prefix
 
 """
